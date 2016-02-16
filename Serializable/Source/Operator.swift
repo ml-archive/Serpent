a2485b8dde7402368fb004c49390032bce357d83
Chris Combs <chco@nodes.dk>
2016-02-16 14:09:02 +0100

    Refactoring Serializable into smaller files

---
+++ Serializable/Source/Operator.swift
@@ -0,0 +1,54 @@
+//
+//  Operator.swift
+//  Serializable
+//
+//  Created by Chris Combs on 16/02/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: - Custom Operators -
+
+infix operator <== { associativity left precedence 90 }
+
+// For Encodable
+public func <==<T>(left: (dict: NSMutableDictionary, key: String), right: T?) {
+	left.dict.setValue(right as? AnyObject, forKey: left.key)
+}
+
+public func <==<T: Encodable>(left: (dict: NSMutableDictionary, key: String), right: T?) {
+	left.dict.setValue(right?.encodableRepresentation(), forKey: left.key)
+}
+
+// For Decodable
+public func <==<T, S where S: Keymappable>(inout left: T?, right: (instance: S, dict: NSDictionary?, key: String)) {
+	let value: T? = right.instance.mapped(right.dict, key: right.key)
+	left = value ?? left
+}
+
+public func <==<T, S where S: Keymappable>(inout left: T, right: (instance: S, dict: NSDictionary?, key: String)) {
+	let value: T? = right.instance.mapped(right.dict, key: right.key)
+	left = value ?? left
+}
+
+public func <==<T, S where T: Decodable, S: Keymappable>(inout left: T?, right: (instance: S, dict: NSDictionary?, key: String)) {
+	let value: T? = right.instance.mapped(right.dict, key: right.key)
+	left = value ?? left
+}
+
+public func <==<T, S where T: Decodable, S: Keymappable>(inout left: T, right: (instance: S, dict: NSDictionary?, key: String)) {
+	let value: T? = right.instance.mapped(right.dict, key: right.key)
+	left = value ?? left
+}
+
+public func <==<T, S where T: RawRepresentable, S: Keymappable>(inout left: T?, right: (instance: S, dict: NSDictionary?, key: String)) {
+	let value: T? = right.instance.mapped(right.dict, key: right.key)
+	left = value ?? left
+}
+
+public func <==<T, S where T: RawRepresentable, S: Keymappable>(inout left: T, right: (instance: S, dict: NSDictionary?, key: String)) {
+	let value: T? = right.instance.mapped(right.dict, key: right.key)
+	left = value ?? left
+}
