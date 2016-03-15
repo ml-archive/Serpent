48c4790414126bec436b969fe0a98cdf8f241dec
Dominik Hadl <nick@dynamicdust.com>
2016-03-15 22:33:00 +0100

    Refactor HexInitializable, add documentation and tweak formatting

--- Serializable/Source/Extensions.swift
+++ Serializable/Source/Extensions.swift
@@ -9,11 +9,33 @@
 import Foundation
 import UIKit
 
+// MARK: - Protocols -
+// MARK: String Initializable
+
+public protocol StringInitializable {
+    init?(string: String)
+}
+
+extension NSURL:StringInitializable {}
+
+// MARK: Hex Initializable
+
+let validHexRegex = try! NSRegularExpression(pattern: "[0-9A-F]{6}", options: NSRegularExpressionOptions(rawValue: 0))
+
+public protocol HexInitializable {
+    static func fromHexString<T>(hexString: String) -> T?
+}
+
+// MARK: - Extensions -
+// MARK: SequenceType
+
 public extension SequenceType where Generator.Element:Encodable {
 	func encodableRepresentation() -> [NSCoding] {
 		return self.map { element in return element.encodableRepresentation() }
 	}
 }
+
+// MARK: RawRepresentable
 
 public extension RawRepresentable {
 	public func encodableRepresentation() -> RawValue {
@@ -21,50 +43,48 @@ public extension RawRepresentable {
 	}
 }
 
+// MARK: UIColor
 
-public protocol StringInitializable {
-	init?(string: String)
-}
-
-extension NSURL:StringInitializable {}
-
-public protocol HexInitializable {
-    static func colorWithHexString (hex:String) -> UIColor?
-}
-
-let validHexRegex = try! NSRegularExpression(pattern: "[0-9A-F]{6}", options: NSRegularExpressionOptions(rawValue: 0))
-
-extension UIColor:HexInitializable {
-    
-    public static func colorWithHexString (hex:String) -> UIColor? {
-        var cString:String = hex.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()).uppercaseString
-        
-        if (cString.hasPrefix("#")) {
-            cString = cString.substringFromIndex(cString.startIndex.advancedBy(1))
-        }
-        
-        if cString.characters.count != 6 {
-            return nil
-        }
-        
-        let range = NSMakeRange(0, 6)
-        
-        if validHexRegex.numberOfMatchesInString(cString, options: NSMatchingOptions(rawValue:0), range: range) != 1 {
-            return nil
-        }
-        
-        let rString = cString.substringToIndex(cString.startIndex.advancedBy(2))
-        let gString = cString.substringWithRange(Range<String.Index>(cString.startIndex.advancedBy(2)..<cString.startIndex.advancedBy(4)))
-        let bString = cString.substringWithRange(Range<String.Index>(cString.startIndex.advancedBy(4)..<cString.startIndex.advancedBy(6)))
-        
-        
-        var r:CUnsignedInt = 0, g:CUnsignedInt = 0, b:CUnsignedInt = 0;
-        NSScanner(string: rString).scanHexInt(&r)
-        NSScanner(string: gString).scanHexInt(&g)
-        NSScanner(string: bString).scanHexInt(&b)
-        
-        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: 1.0)
+extension UIColor {
+    public convenience init?(hexString: String) {
+        self.init(red: 0, green: 0, blue: 0, alpha: 0)
     }
 }
 
+extension UIColor: HexInitializable {
+    public static func fromHexString<T>(hexString: String) -> T? {
+        let charSet = NSCharacterSet.whitespaceAndNewlineCharacterSet()
+        var str = hexString.stringByTrimmingCharactersInSet(charSet).uppercaseString
+
+        if (str.hasPrefix("#")) {
+            str = str.substringFromIndex(str.startIndex.advancedBy(1))
+        }
+
+        // TODO: Handle 3, 4 & 8 character long HEX strings in the future
+        if str.characters.count != 6 {
+            return nil
+        }
+
+        let range   = NSMakeRange(0, 6)
+        let options = NSMatchingOptions(rawValue:0)
+
+        guard validHexRegex.numberOfMatchesInString(str, options: options, range: range) == 1 else {
+            return nil
+        }
+
+        let rString = str.substringToIndex(str.startIndex.advancedBy(2))
+        let gString = str.substringWithRange(str.startIndex.advancedBy(2)..<str.startIndex.advancedBy(4))
+        let bString = str.substringWithRange(str.startIndex.advancedBy(4)..<str.startIndex.advancedBy(6))
+
+        var r: CUnsignedInt = 0, g: CUnsignedInt = 0, b: CUnsignedInt = 0
+        NSScanner(string: rString).scanHexInt(&r)
+        NSScanner(string: gString).scanHexInt(&g)
+        NSScanner(string: bString).scanHexInt(&b)
+
+        return UIColor(
+            red: CGFloat(r) / 255.0,
+            green: CGFloat(g) / 255.0,
+            blue: CGFloat(b) / 255.0,
+            alpha: 1.0) as? T
+    }
 }
