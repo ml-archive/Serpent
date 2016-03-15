6365d258866b813350608b63069369c575adb6cf
Chris Combs <chco@nodes.dk>
2016-03-15 13:09:51 +0100

    Turns out these functions were important

--- Serializable/Source/Extensions.swift
+++ Serializable/Source/Extensions.swift
@@ -9,8 +9,6 @@
 import Foundation
 import UIKit
 
-/*
-	I don't think these are needed anymore?
 
 public extension Array where Element:Encodable {
 	func encodableRepresentation() -> [NSCoding] {
@@ -23,7 +21,7 @@ public extension RawRepresentable {
 		return self.rawValue
 	}
 }
-*/
+
 
 public protocol StringInitializable {
 	init?(string: String)
