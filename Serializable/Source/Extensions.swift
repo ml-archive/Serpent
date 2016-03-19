7326242c8e5cab7b464e7cd13ff74b96c0a92e48
Chris Combs <chco@nodes.dk>
2016-03-19 21:21:34 +0100

    Added OSX, watchOS and tvOS targets, tests need some refactoring to support colors

--- Serializable/Source/Extensions.swift
+++ Serializable/Source/Extensions.swift
@@ -7,7 +7,6 @@
 //
 
 import Foundation
-import UIKit
 
 // MARK: - Protocols -
 // MARK: String Initializable
@@ -73,42 +72,3 @@ public extension RawRepresentable {
 	}
 }
 
-// MARK: UIColor
-
-extension UIColor: HexInitializable {
-    public static func fromHexString<T>(hexString: String) -> T? {
-        let charSet = NSCharacterSet.whitespaceAndNewlineCharacterSet()
-        var str = hexString.stringByTrimmingCharactersInSet(charSet).uppercaseString
-
-        if (str.hasPrefix("#")) {
-            str = str.substringFromIndex(str.startIndex.advancedBy(1))
-        }
-
-        // TODO: Handle 3, 4 & 8 character long HEX strings in the future
-        if str.characters.count != 6 {
-            return nil
-        }
-
-        let range   = NSMakeRange(0, 6)
-        let options = NSMatchingOptions(rawValue:0)
-
-        guard validHexRegex.numberOfMatchesInString(str, options: options, range: range) == 1 else {
-            return nil
-        }
-
-        let rString = str.substringToIndex(str.startIndex.advancedBy(2))
-        let gString = str.substringWithRange(str.startIndex.advancedBy(2)..<str.startIndex.advancedBy(4))
-        let bString = str.substringWithRange(str.startIndex.advancedBy(4)..<str.startIndex.advancedBy(6))
-
-        var r: CUnsignedInt = 0, g: CUnsignedInt = 0, b: CUnsignedInt = 0
-        NSScanner(string: rString).scanHexInt(&r)
-        NSScanner(string: gString).scanHexInt(&g)
-        NSScanner(string: bString).scanHexInt(&b)
-
-        return self.init(
-            red: CGFloat(r) / 255.0,
-            green: CGFloat(g) / 255.0,
-            blue: CGFloat(b) / 255.0,
-            alpha: 1.0) as? T
-    }
-}
