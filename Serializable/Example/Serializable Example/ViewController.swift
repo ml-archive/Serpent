8ead35a4d9645d96a1bbaa2d82f8570730d46d02
Dominik Hadl <nick@dynamicdust.com>
2016-04-17 11:27:48 +0200

    Update and cleanup project structure, prepare for example project

---
+++ Serializable/Example/Serializable Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Serializable Example
+//
+//  Created by Dominik Hádl on 17/04/16.
+//  Copyright © 2016 Nodes ApS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
