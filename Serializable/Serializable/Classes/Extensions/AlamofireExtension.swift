2e55e1a91e1e418a81d899a58783eabdc4de0214
Jakob Mygind <Jmje@nodes.dk>
2016-09-09 10:15:19 +0200

    Swift 3.0 update

--- Serializable/Serializable/Classes/Extensions/AlamofireExtension.swift
+++ Serializable/Serializable/Classes/Extensions/AlamofireExtension.swift
@@ -73,7 +73,7 @@ public extension Alamofire.Request
      - returns: The request
      */
     @discardableResult
-    public func responseSerializable<T:Decodable>(_ completionHandler: @escaping (Response<T, NSError>) -> Void, unwrapper:Parser.Unwrapper = Parser.defaultUnwrapper) -> Self {
+    public func responseSerializable<T:Decodable>(_ completionHandler: @escaping (Response<T, NSError>) -> Void, unwrapper:@escaping Parser.Unwrapper = Parser.defaultUnwrapper) -> Self {
         let serializer = Parser.serializer(parsingHandler: {
             ( data: Any? ) -> T? in
             
@@ -102,7 +102,7 @@ public extension Alamofire.Request
      - returns: The request
      */
 	@discardableResult
-    public func responseSerializable<T:Decodable>(_ completionHandler: @escaping (Response<[T], NSError>) -> Void, unwrapper:Parser.Unwrapper = Parser.defaultUnwrapper) -> Self {
+    public func responseSerializable<T:Decodable>(_ completionHandler: @escaping (Response<[T], NSError>) -> Void, unwrapper:@escaping Parser.Unwrapper = Parser.defaultUnwrapper) -> Self {
         
         let serializer = Parser.serializer(parsingHandler: {
             ( data: Any? ) -> [T]? in
